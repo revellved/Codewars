be597302ca2d8bc33cfd970c62ae30ea62155e13
Ruslan D. Revellved <revellved@gmail.com>
2024-06-08 17:14:56 +0500

    Alphabet Position

---
+++ rust/src/alphabet_position/mod.rs
@@ -0,0 +1,42 @@
+use std::char;
+
+fn _alphabet_position(text: &str) -> String {
+    let mut res = String::from("");
+    text.to_lowercase().chars().for_each(|c: char| {
+        let v = c as i64 - 96;
+        if v > 0 && v < 27 {
+            if res.len() != 0 {
+                res += " "
+            }
+            res += &(v).to_string();
+        }
+    });
+
+    res
+}
+
+pub fn alphabet_position(text: &str) -> String {
+    text.to_lowercase()
+        .chars()
+        .filter(|c| c >= &'a' && c <= &'z')
+        .map(|c| (c as u32 - 96).to_string())
+        .collect::<Vec<String>>()
+        .join(" ")
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn returns_expected() {
+        assert_eq!(
+            alphabet_position("The sunset sets at twelve o' clock."),
+            "20 8 5 19 21 14 19 5 20 19 5 20 19 1 20 20 23 5 12 22 5 15 3 12 15 3 11".to_string()
+        );
+        assert_eq!(
+            alphabet_position("The narwhal bacons at midnight."),
+            "20 8 5 14 1 18 23 8 1 12 2 1 3 15 14 19 1 20 13 9 4 14 9 7 8 20".to_string()
+        );
+    }
+}
--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,8 +1,15 @@
+use alphabet_position::alphabet_position;
 use hello::hello;
 
+mod alphabet_position;
 mod hello;
 
 fn main() {
-    let text = hello();
+    let text = hello().clone();
     println!("{text}");
+
+    assert_eq!(
+        alphabet_position(text.as_str()),
+        "8 5 12 12 15 23 15 18 12 4"
+    );
 }
