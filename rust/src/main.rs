29c564eb82d9ab2c8989db18597faa901307a4ee
Ruslan D. Revellved <revellved@gmail.com>
2024-06-08 17:28:06 +0500

    Unique In Order

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -4,12 +4,14 @@ use count_chars::count_chars;
 use count_duplicates::count_duplicates;
 use digital_root::digital_root;
 use hello::hello;
+use unique_in_order::unique_in_order;
 
 mod alphabet_position;
 mod count_chars;
 mod count_duplicates;
 mod digital_root;
 mod hello;
+mod unique_in_order;
 
 fn main() {
     let text = hello().clone();
@@ -22,4 +24,8 @@ fn main() {
     assert_eq!(digital_root(16), 7);
     assert_eq!(count_duplicates("abcde"), 0);
     assert_eq!(count_chars("aba"), map_key_val!('a': 2, 'b': 1));
+    assert_eq!(
+        unique_in_order("AAAABBBCCDAABBB".chars()),
+        vec!['A', 'B', 'C', 'D', 'A', 'B']
+    );
 }
