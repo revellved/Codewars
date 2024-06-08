7a518240a8a9ccc1110ec9c3461eb135240f81c1
Ruslan D. Revellved <revellved@gmail.com>
2024-06-08 17:31:34 +0500

    Add two macros to lib.rs

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -9,3 +9,23 @@ macro_rules! map_key_val {
         map
     }};
 }
+
+#[macro_export]
+macro_rules! map_key_and_expr {
+    () => { HashMap::new() };
+    ($($key:ident : $val:expr),+) => {{
+        let mut map = $crate::HashMap::new();
+        $( map.insert(stringify!($key), $val); )*
+        map
+    }};
+}
+
+#[macro_export]
+macro_rules! map_ss {
+        () => { HashMap::new() };
+        ($($key:literal : $value:literal),+) => {{
+            let mut map = HashMap::new();
+            $( map.insert(String::from_str($key).unwrap(), String::from_str($value).unwrap()); )*
+            map
+        }};
+    }
