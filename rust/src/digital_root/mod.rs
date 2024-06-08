139442ea12adabe180589bc20342fabb0aa729c5
Ruslan D. Revellved <revellved@gmail.com>
2024-06-08 17:14:56 +0500

    Digital Root

---
+++ rust/src/digital_root/mod.rs
@@ -0,0 +1,49 @@
+fn _1_digital_root(n: i64) -> i64 {
+    let str_num = n.to_string();
+    let mut sum_nums: i64 = 0;
+
+    for i in str_num.chars() {
+        sum_nums += i as i64 - 48;
+    }
+
+    return sum_nums;
+}
+
+fn _sum_ordire_enumarate(n: i64) -> i64 {
+    n.to_string().chars().fold(0, |mut sum, c| {
+        sum += c as i64 - 48;
+        sum
+    })
+}
+
+fn _2_digital_root(mut n: i64) -> i64 {
+    while n >= 10 {
+        n = _sum_ordire_enumarate(n)
+    }
+    n
+}
+
+fn _3_digital_root(mut n: i64) -> i64 {
+    while n >= 10 {
+        n = n.to_string().chars().fold(0, |mut sum, c| {
+            sum += c as i64 - 48;
+            sum
+        })
+    }
+    n
+}
+
+pub fn digital_root(n: i64) -> i64 {
+    (n - 1) % 9 + 1
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn returns_expected() {
+        assert_eq!(digital_root(16), 7);
+        assert_eq!(digital_root(942), 6);
+    }
+}
--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,8 +1,18 @@
+use alphabet_position::alphabet_position;
+use digital_root::digital_root;
 use hello::hello;
 
+mod alphabet_position;
+mod digital_root;
 mod hello;
 
 fn main() {
-    let text = hello();
+    let text = hello().clone();
     println!("{text}");
+
+    assert_eq!(
+        alphabet_position(text.as_str()),
+        "8 5 12 12 15 23 15 18 12 4"
+    );
+    assert_eq!(digital_root(16), 7);
 }
