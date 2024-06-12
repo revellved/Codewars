3e8365270c10647f62dd5c735ea40e8f853851dd
Ruslan D. Revellved <revellved@gmail.com>
2024-06-12 13:27:41 +0500

    (RUST) Pretty Next Smaller Number

--- rust/src/modules/next_smaller_number/mod.rs
+++ rust/src/modules/next_smaller_number/mod.rs
@@ -1,4 +1,27 @@
 pub fn next_smaller_number(n: u64) -> Option<u64> {
+    let mut digs = n
+        .to_string()
+        .chars()
+        .map(|c| c.to_digit(10).unwrap())
+        .collect::<Vec<u32>>();
+    let bnp = digs.windows(2).rposition(|d| d[0] > d[1])?;
+    let bn = digs[bnp];
+    let mn = digs[bnp + 1..].iter().filter(|&d| d < &bn).max()?;
+    let mnp = digs.iter().rposition(|&d| d == *mn)?;
+    digs.swap(bnp, mnp);
+    let (_, tail) = digs.split_at_mut(bnp + 1);
+    tail.reverse();
+    if digs[0] == 0 {
+        return None;
+    }
+    digs.into_iter()
+        .map(|d| std::char::from_digit(d, 10).unwrap())
+        .collect::<String>()
+        .parse::<u64>()
+        .ok()
+}
+
+pub fn _next_smaller_number(n: u64) -> Option<u64> {
     let mut digits: Vec<u64> = n
         .to_string()
         .chars()
