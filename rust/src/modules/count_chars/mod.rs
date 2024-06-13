570d43b88b4b1b10d36558bb8dae7295e57840e0
Ruslan D. Revellved <revellved@gmail.com>
2024-06-13 07:17:40 +0500

    (RUST) Count Chars add kata name, link and description

--- rust/src/modules/count_chars/mod.rs
+++ rust/src/modules/count_chars/mod.rs
@@ -1,4 +1,14 @@
-use std::collections::HashMap;
+// Count characters in your string
+// https://www.codewars.com/kata/52efefcbcdf57161d4000091
+//
+// Description:
+//
+// The main idea is to count all the occurring characters in a string. If you have a string like aba, then the result should be {'a': 2, 'b': 1}.
+//
+// What if the string is empty? Then the result should be empty object literal, {}.
+//
+// use std::collections::HashMap;
+//
 
 pub fn count_chars(input: &str) -> HashMap<char, i32> {
     let mut used_chars: HashMap<char, i32> = HashMap::new();
