691e6ef017ee5941bb2447e6e0133d062aeaa238
Ruslan D. Revellved <revellved@gmail.com>
2024-06-13 07:07:56 +0500

    (RUST) Multiplies of 3 or 5

---
+++ rust/src/modules/multiples_of_3_or_5/mod.rs
@@ -0,0 +1,68 @@
+// Multiples of 3 or 5
+// https://www.codewars.com/kata/514b92a657cdc65150000006/rust
+//
+// Description:
+//
+// If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+//
+// Finish the solution so that it returns the sum of all the multiples of 3 or 5 below the number passed in.
+//
+// Additionally, if the number is negative, return 0.
+//
+// Note: If the number is a multiple of both 3 and 5, only count it once.
+//
+// Courtesy of projecteuler.net (Problem 1)
+//
+
+pub fn solution(num: i32) -> i32 {
+    (1..num).filter(|x| x % 3 == 0 || x % 5 == 0).sum()
+}
+
+pub fn _solution(num: i32) -> i32 {
+    let mut sum = 0;
+
+    let max_3 = num / 3 + std::cmp::min(num % 3, 1);
+    let max_5 = num / 5 + std::cmp::min(num % 5, 1);
+
+    for i in 1..num {
+        if i < max_3 {
+            sum += i * 3;
+        };
+        if i < max_5 && i % 3 != 0 {
+            sum += i * 5;
+        };
+    }
+
+    sum
+}
+
+mod tests {
+    use super::solution;
+
+    #[test]
+    fn sample_tests() {
+        // assertion(expected, input);
+        assertion(23, 10);
+        assertion(33, 11);
+        assertion(225, 33);
+        assertion(8, 6);
+        assertion(3420, 123);
+        assertion(543, 50);
+        assertion(0, 0);
+        assertion(0, -203);
+        assertion(25719750, 10500);
+    }
+
+    #[allow(dead_code)]
+    fn assertion(expected: i32, input: i32) {
+        let actual = solution(input);
+
+        assert!(
+            expected == actual,
+            "\nTest failed!\n expected: {}\n actual: {}\n input: {}\n",
+            expected,
+            actual,
+            input
+        );
+    }
+}
