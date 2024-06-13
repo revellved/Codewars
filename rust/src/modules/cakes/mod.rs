746a4167bb0d6d49c7943c8e382560264cd8adbe
Ruslan D. Revellved <revellved@gmail.com>
2024-06-13 07:41:31 +0500

    (RUST) moved names, links and descriptions of kata into separate .md files

--- rust/src/modules/cakes/mod.rs
+++ rust/src/modules/cakes/mod.rs
@@ -1,20 +1,3 @@
-// Pete, the baker
-// https://www.codewars.com/kata/525c65e51bf619685c000059
-//
-// Description:
-//
-// Pete likes to bake some cakes. He has some recipes and ingredients. Unfortunately he is not good in maths. Can you help him to find out, how many cakes he could bake considering his recipes?
-//
-// Write a function cakes(), which takes the recipe (HashMap<&str, u32>) and the available ingredients (also a HashMap<&str, u32>) and returns the maximum number of cakes Pete can bake (u32). For simplicity there are no units for the amounts (e.g. 1 lb of flour or 200 g of sugar are simply 1 or 200). Ingredients that are not present in the objects, can be considered as 0.
-//
-// Examples:
-//
-// // must return 2
-// cakes(HashMap::from([("flour", 500), ("sugar", 200), ("eggs", 1)]), HashMap::from([("flour", 1200), ("sugar", 1200), ("eggs", 5), ("milk", 200)]))
-// // must return 0
-// cakes(HashMap::from([("apples", 3), ("flour", 300), ("sugar", 150), ("milk", 100), ("oil", 100)]), HashMap::from([("sugar", 500),("flour", 2000), ("milk", 2000)]))
-//
-
 use std::collections::HashMap;
 
 pub fn cakes(recipe: &HashMap<&str, u32>, available: &HashMap<&str, u32>) -> u32 {
--- rust/src/modules/check_the_exam/mod.rs
+++ rust/src/modules/check_the_exam/mod.rs
@@ -1,24 +1,3 @@
-// Check the exam
-// https://www.codewars.com/kata/5a3dd29055519e23ec000074
-//
-// Description:
-//
-// The first input array is the key to the correct answers to an exam, like ["a", "a", "b", "d"]. The second one contains a student's submitted answers.
-//
-// The two arrays are not empty and are the same length. Return the score for this array of answers, giving +4 for each correct answer, -1 for each incorrect answer, and +0 for each blank answer, represented as an empty string (in C the space character is used).
-//
-// If the score < 0, return 0.
-//
-// For example:
-//
-//     Correct answer    |    Student's answer   |   Result
-//  ---------------------|-----------------------|-----------
-//  ["a", "a", "b", "b"]   ["a", "c", "b", "d"]  →     6
-//  ["a", "a", "c", "b"]   ["a", "a", "b", "" ]  →     7
-//  ["a", "a", "b", "c"]   ["a", "a", "b", "c"]  →     16
-//  ["b", "c", "b", "a"]   ["" , "a", "a", "c"]  →     0
-//
-
 pub fn check_exam(arr_a: &[&str], arr_b: &[&str]) -> i64 {
     arr_b
         .iter()
