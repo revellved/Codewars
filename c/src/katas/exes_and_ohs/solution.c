85db61833b6df9a4953bfd3db57a5f42cfb7aebb
Ruslan D. Revellved <revellved@gmail.com>
2024-06-17 14:14:11 +0500

    (C) Exes And Ohs update solution

--- c/src/katas/exes_and_ohs/solution.c
+++ c/src/katas/exes_and_ohs/solution.c
@@ -3,7 +3,7 @@
 
 bool xo(const char *str) {
   unsigned x = 0, o = 0;
-  for (char *p = str; *p; p++) {
+  for (const char *p = str; *p; p++) {
     if (tolower(*p) == 'x')
       x++;
     else if (tolower(*p) == 'o')
