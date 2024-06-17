c948f4d27c0867c722695ec7a8fb552b1985fe1a
Ruslan D. Revellved <revellved@gmail.com>
2024-06-17 13:23:12 +0500

    (C) Square Digits Update

--- c/src/katas/square_digits/solution.c
+++ c/src/katas/square_digits/solution.c
@@ -1,18 +1,11 @@
-#include <math.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-
 unsigned long long square_digits(unsigned n) {
-  char str_sum[50] = "";
-  while (n != 0) {
-    unsigned num = pow(n % 10, 2);
-    char temp_str[50];
-    sprintf(temp_str, "%d", num);
-    strcat(temp_str, str_sum);
-    sprintf(str_sum, "%s", temp_str);
-
-    n /= 10;
-  }
-  return strtoull(str_sum, NULL, 10);
+  unsigned long long q = 0;
+  unsigned long long m = 1;
+  unsigned r;
+  do {
+    r = n % 10;
+    q += r * r * m;
+    m *= r > 3 ? 100 : 10;
+  } while (n /= 10);
+  return q;
 }
