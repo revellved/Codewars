ab7c83a1406903c0507fcfede42d9eacc60d0c7f
Ruslan D. Revellved <revellved@gmail.com>
2024-06-17 13:52:36 +0500

    (C) Sum Two Lowest Nums

---
+++ c/src/katas/sum_two_lowest_nums/solution.c
@@ -0,0 +1,20 @@
+#include <limits.h>
+#include <stddef.h>
+
+long sum_two_smallest_numbers(size_t n, const int numbers[n]) {
+  int n1 = INT_MAX;
+  int n2 = INT_MAX;
+
+  for (size_t i = 0; i < n; i++) {
+    if (numbers[i] < n1) {
+      if (n1 < n2) {
+        n2 = n1;
+      }
+      n1 = numbers[i];
+    } else if (numbers[i] < n2) {
+      n2 = numbers[i];
+    }
+  }
+
+  return n1 + n2;
+}
