9734652de0f7560f3cc839973140836e035c2194
Ruslan D. Revellved <revellved@gmail.com>
2024-06-19 00:18:32 +0500

    (RUBY) Rspec configuration

---
+++ ruby/spec/spec_helper.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require 'application'
+require 'codewars_test'
+
+RSpec.configure do |config|
+  config.shared_context_metadata_behavior = :apply_to_host_groups
+end
