3245471612274ad7af6d212c56f321b32e2a128b
Ruslan D. Revellved <revellved@gmail.com>
2024-06-19 00:21:31 +0500

    (CPP) Deleted

--- cpp/src/katas/testing-1-2-3/solution.cpp
+++
@@ -1,6 +0,0 @@
-#include <string>
-#include <vector>
-
-std::vector<std::string> number(const std::vector<std::string> &lines) {
-  return {};
-}
--- cpp/src/main.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-
-using namespace std;
-
-int main() {
-  cout << "Hellooo, World!" << endl;
-
-  return 0;
-}
