e3df21e5ee34226317c9f257a677c1484894578e
Ruslan D. Revellved <revellved@gmail.com>
2024-06-19 18:51:42 +0500

    (RUBY) WeIrD StRiNg CaSe (https://www.codewars.com/kata/52b757663a95b11b3d00062d/ruby) [UPDATE]

--- ruby/lib/katas/weird_string_case/solution.rb
+++ ruby/lib/katas/weird_string_case/solution.rb
@@ -6,10 +6,6 @@ module WeirdStringCase
 
   sig { params(string: String).returns(String) }
   def weirdcase(string)
-    string.split.map do |a|
-      a.chars.each_slice(2).reduce('') do |acc, pairs|
-        acc + pairs[0].to_s.upcase + pairs[1].to_s.downcase
-      end
-    end.join(' ')
+    string.gsub(/\w{1,2}/) { ::Regexp.last_match(0).capitalize }
   end
 end
