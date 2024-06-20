1fdbafd31137a292b6b5c198e2798a5c64cc706c
Ruslan D. Revellved <revellved@gmail.com>
2024-06-20 22:45:35 +0500

    (JAVA) ReverseWords [UPDATE]

--- java/src/main/java/com/revellved/codewars/katas/ReverseWords.java
+++ java/src/main/java/com/revellved/codewars/katas/ReverseWords.java
@@ -1,21 +1,19 @@
-package com.revellved.codewars;
+package com.revellved.codewars.katas;
 
 /**
  * reverse Words
  *
  */
 
-public class ReverseWords
-{
-  public static String exec(final String original)
-  {
+public class ReverseWords {
+  public static String exec(final String original) {
     String reverseStr = "";
     for (String part : original.split(" ")) {
-        reverseStr += new StringBuilder(part).reverse().toString() + " ";
+      reverseStr += new StringBuilder(part).reverse().toString() + " ";
     }
     if (reverseStr.length() == 0) {
       return original;
     }
-    return reverseStr.substring(0, reverseStr.length()-1);
+    return reverseStr.substring(0, reverseStr.length() - 1);
   }
 }
--- java/src/test/java/com/revellved/codewars/katas/ReverseWordsTest.java
+++ java/src/test/java/com/revellved/codewars/katas/ReverseWordsTest.java
@@ -1,4 +1,4 @@
-package com.revellved.codewars;
+package com.revellved.codewars.katas;
 
 import org.junit.Test;
 import static org.junit.Assert.assertEquals;
@@ -7,13 +7,14 @@ import org.junit.runners.JUnit4;
 // TODO: Replace examples and use TDD development by writing your own tests
 
 public class ReverseWordsTest {
-    @Test
-    public void exampleCases() {
-        assertEquals("ehT kciuq nworb xof spmuj revo eht yzal .god", ReverseWords.exec("The quick brown fox jumps over the lazy dog."));
-        assertEquals("elppa", ReverseWords.exec("apple"));
-        assertEquals("a b c d", ReverseWords.exec("a b c d"));
-        assertEquals("elbuod  decaps  sdrow", ReverseWords.exec("double  spaced  words"));
-        assertEquals("", ReverseWords.exec(""));
-        assertEquals("     ", ReverseWords.exec("     "));
-    }
+  @Test
+  public void exampleCases() {
+    assertEquals("ehT kciuq nworb xof spmuj revo eht yzal .god",
+        ReverseWords.exec("The quick brown fox jumps over the lazy dog."));
+    assertEquals("elppa", ReverseWords.exec("apple"));
+    assertEquals("a b c d", ReverseWords.exec("a b c d"));
+    assertEquals("elbuod  decaps  sdrow", ReverseWords.exec("double  spaced  words"));
+    assertEquals("", ReverseWords.exec(""));
+    assertEquals("     ", ReverseWords.exec("     "));
+  }
 }
