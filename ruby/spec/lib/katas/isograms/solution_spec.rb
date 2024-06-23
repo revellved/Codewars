b1a658dc7f836ec16484fbe510a01591603e711d
Ruslan D. Revellved <revellved@gmail.com>
2024-06-23 20:40:44 +0500

    (RUBY) Spec for "Isograms" (https://www.codewars.com/kata/54ba84be607a92aa900000f1/ruby)

---
+++ ruby/spec/lib/katas/isograms/solution_spec.rb
@@ -0,0 +1,12 @@
+describe "Solution" do
+  it "Fixed tests" do
+    extend Isograms
+
+    Test.assert_equals(is_isogram("Dermatoglyphics"), true )
+    Test.assert_equals(is_isogram("isogram"), true )
+    Test.assert_equals(is_isogram("aba"), false, "same chars may not be adjacent" )
+    Test.assert_equals(is_isogram("moOse"), false, "same chars may not be same case" )
+    Test.assert_equals(is_isogram("isIsogram"), false )
+    Test.assert_equals(is_isogram(""), true, "an empty string is a valid isogram" )
+  end
+end
