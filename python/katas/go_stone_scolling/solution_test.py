76942e130e7f63eb5d470b5dcdfd0a8019fecdaf
Ruslan D. Revellved <revellved@gmail.com>
2024-06-24 14:47:32 +0500

    (PYTHON) Test for "Go: Stone Scoring" (https://www.codewars.com/kata/5fdb2ef8656423001c00e648/python)

---
+++ python/katas/go_stone_scolling/solution_test.py
@@ -0,0 +1,8 @@
+import codewars_test as test
+import katas.go_stone_scolling.solution as solution
+
+@test.describe("6x6 Boards")
+def test_group():
+    @test.it("6x6 Board 1")
+    def test_case():
+        test.assert_equals(solution.determine_winner([["W","W","W","B","B","B"], ["W","W","W","W","B","B"], ["W","W","W","B","B","B"], ["W","X","W","B","B","B"], ["X","W","B","B","B","B"], ["W","W","B","X","B","X"]]), ("B", 17))
