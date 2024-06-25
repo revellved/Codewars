ee4f902b03ca373cb370014faa319a7fd43193a1
Ruslan D. Revellved <revellved@gmail.com>
2024-06-25 15:06:09 +0500

    (PYTHON) 🍩 Update katas/take_ten_minutes_walk/solution_test.py

--- python/katas/take_ten_minutes_walk/solution_test.py
+++ python/katas/take_ten_minutes_walk/solution_test.py
@@ -5,13 +5,32 @@ from . solution import is_valid_walk
 def sample_tests():
     @test.it ("should return true for a valid walk")
     def _():
-        test.assert_equals(is_valid_walk(['n','s','n','s','n','s','n','s','n','s']), True, 'should return True');
+        test.assert_equals(
+            is_valid_walk(['n','s','n','s','n','s','n','s','n','s']), 
+            True,
+            'should return True'
+        );
+    
     @test.it ("should return false if walk is too long")
     def _():
-        test.assert_equals(is_valid_walk(['w','e','w','e','w','e','w','e','w','e','w','e']), False, 'should return False');
+        test.assert_equals(
+            is_valid_walk(['w','e','w','e','w','e','w','e','w','e','w','e']), 
+            False, 
+            'should return False'
+        );
+    
     @test.it ("should return false if walk is too short")
     def _():
-        test.assert_equals(is_valid_walk(['w']), False, 'should return False');
+        test.assert_equals(
+            is_valid_walk(['w']), 
+            False, 
+            'should return False'
+        );
+    
     @test.it ("should return false if walk does not bring you back to start")
     def _():        
-        test.assert_equals(is_valid_walk(['n','n','n','s','n','s','n','s','n','s']), False, 'should return False');
+        test.assert_equals(
+            is_valid_walk(['n','n','n','s','n','s','n','s','n','s']),
+            False,
+            'should return False'
+        );
