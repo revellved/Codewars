49e243faec7968e205e661f455f7da382d141906
Ruslan D. Revellved <revellved@gmail.com>
2024-06-25 15:21:16 +0500

    (PYTHON) 🍩 Update katas/hashtag_generator/solution.py

--- python/katas/hashtag_generator/solution.py
+++ python/katas/hashtag_generator/solution.py
@@ -1,5 +1,5 @@
 def generate_hashtag(s: str) -> str|bool:
-    if not s or len(s) > 140:
-        return False
     hashtag = '#' + ''.join(word.capitalize() for word in s.split())
+    if not s or len(hashtag) > 140:
+        return False
     return hashtag
