457b7f963ae3aedd756bec9e2f4a48613a5b00f4
Ruslan D. Revellved <revellved@gmail.com>
2024-06-25 15:02:11 +0500

    (PYTHON) 🍩 Update katas/dont_give_me_five/solution_test.py

--- python/katas/dont_give_me_five/solution_test.py
+++ python/katas/dont_give_me_five/solution_test.py
@@ -1,9 +1,9 @@
 import codewars_test as test
 from . solution import dont_give_me_five
 
-@test.describe("Fixed Tests")
+@test.describe("Dont Give Me Five")
 def fixed_tests():
     @test.it('Basic Test Cases')
-    def basic_test_cases():
+    def _():
         test.assert_equals(dont_give_me_five(1,9), 8)
         test.assert_equals(dont_give_me_five(4,17), 12)
