4b6898019a43f465d303c4e54078c60b6277fe43
Ruslan D. Revellved <revellved@gmail.com>
2024-06-25 21:35:53 +0500

    (TS)   🍩 Newest

---
+++ ts/src/katas/tribonacci_sequence/solution.ts
@@ -0,0 +1,13 @@
+export function tribonacci(signature: number[], n: number): number[] {
+    if (n === 0) {
+        return [];
+    }
+
+    const result: number[] = signature.slice(0, n);
+    
+    for (let i = 3; i < n; i++) {
+        result[i] = result[i - 1] + result[i - 2] + result[i - 3];
+    }
+
+    return result;
+}
