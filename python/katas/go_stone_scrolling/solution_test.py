fa20e546d438ae7613acdeb9680ab97db5562149
Ruslan D. Revellved <revellved@gmail.com>
2024-06-25 08:33:07 +0500

    (PYTHON) pretty format of test for go stone scrolling

--- python/katas/go_stone_scrolling/solution_test.py
+++ python/katas/go_stone_scrolling/solution_test.py
@@ -5,4 +5,11 @@ import katas.go_stone_scrolling.solution as solution
 def test_group():
     @test.it("6x6 Board 1")
     def test_case():
-        test.assert_equals(solution.determine_winner([["W","W","W","B","B","B"], ["W","W","W","W","B","B"], ["W","W","W","B","B","B"], ["W","X","W","B","B","B"], ["X","W","B","B","B","B"], ["W","W","B","X","B","X"]]), ("B", 17))
+        test.assert_equals(solution.determine_winner(
+            [["W","W","W","B","B","B"], 
+             ["W","W","W","W","B","B"], 
+             ["W","W","W","B","B","B"], 
+             ["W","X","W","B","B","B"], 
+             ["X","W","B","B","B","B"], 
+             ["W","W","B","X","B","X"]]
+        ), ("B", 17))
