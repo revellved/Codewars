6eb45f146faefd23a596063b9f5a09ceb24f0d40
Ruslan D. Revellved <revellved@gmail.com>
2024-06-25 08:25:12 +0500

    (PYTHON) declare types go stone scrolling

--- python/katas/go_stone_scolling/solution.py
+++ python/katas/go_stone_scolling/solution.py
@@ -1,6 +1,9 @@
-def determine_winner(board):
-    black_count = sum(row.count("B") for row in board)
-    white_count = sum(row.count("W") for row in board)
+from typing import List
+
+
+def determine_winner(board: List[str]) -> tuple[str, int]:
+    black_count: int = sum(row.count("B") for row in board)
+    white_count: int = sum(row.count("W") for row in board)
 
     if black_count > white_count:
         return ("B", black_count)
