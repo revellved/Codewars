fcb2dd627b6d70e5d4c6ddfc797f30dac4f5e443
Ruslan D. Revellved <revellved@gmail.com>
2024-06-25 08:24:51 +0500

    (PYTHON) declare types for fortune

--- python/katas/fortune/solution.py
+++ python/katas/fortune/solution.py
@@ -1,7 +1,8 @@
-def fortune(f, p, c, n, i):
+def fortune(f: int, p: int, c: int, n: int, i: int) -> bool:
     for _ in range(n-1):
         f = int(f * (100 + p) / 100 - c)
         c = int(c * (100 + i) / 100)
         if f < 0:
             return False
     return True
+
