e54a2bb6d4ebe14dc79d9f433c119ec323cf0845
Ruslan D. Revellved <revellved@gmail.com>
2024-06-25 15:02:42 +0500

    (PYTHON) 🍩 Update katas/is_square/solution_test.py

--- python/katas/is_square/solution_test.py
+++ python/katas/is_square/solution_test.py
@@ -1,10 +1,10 @@
 import codewars_test as test
 from . solution import is_square
 
-@test.describe("Fixed Tests")
+@test.describe("Is Square")
 def fixed_tests():
     @test.it('Basic Test Cases')
-    def basic_test_cases():
+    def _():
         test.assert_equals(is_square(-1), False, "-1: Negative numbers cannot be square numbers")
         test.assert_equals(is_square( 0), True, "0 is a square number (0 * 0)")
         test.assert_equals(is_square( 3), False, "3 is not a square number")
