fd13a9b50d097b9b8b3c30f3005c37018fdb99ad
Ruslan D. Revellved <revellved@gmail.com>
2024-06-25 09:22:11 +0500

    (PYTHON) pretty format of test for list_filtering

--- python/katas/list_filtering/solution_test.py
+++ python/katas/list_filtering/solution_test.py
@@ -5,7 +5,19 @@ from . solution import filter_list
 def sample_tests():
     @test.it('should work for basic examples')
     def _():
-        test.assert_equals(filter_list([1, 2, 'a', 'b']), [1, 2], 'For input [1, 2, "a", "b"]')
-        test.assert_equals(filter_list([1, 'a', 'b', 0, 15]), [1, 0, 15], 'Fot input [1, "a", "b", 0, 15]')
-        test.assert_equals(filter_list([1, 2, 'aasf', '1', '123', 123]), [1, 2, 123], 'For input [1, 2, "aasf", "1", "123", 123]')
+        test.assert_equals(
+            filter_list([1, 2, 'a', 'b']), 
+            [1, 2], 
+            'For input [1, 2, "a", "b"]'
+        )
+        test.assert_equals(
+            filter_list([1, 'a', 'b', 0, 15]), 
+            [1, 0, 15], 
+            'Fot input [1, "a", "b", 0, 15]'
+        )
+        test.assert_equals(
+            filter_list( [1, 2, 'aasf', '1', '123', 123]), 
+            [1, 2, 123], 
+            'For input [1, 2, "aasf", "1", "123", 123]'
+        )
 
