6806cc2e82dd048d02a6f3af21520a958956e7c0
Ruslan D. Revellved <revellved@gmail.com>
2024-06-27 09:58:55 +0500

    (C) 🍩 Update src/katas/disemvowel_trolls/solution.c

--- c/src/katas/disemvowel_trolls/solution.c
+++ c/src/katas/disemvowel_trolls/solution.c
@@ -1,17 +1,9 @@
 #include <stdlib.h>
 #include <string.h>
 
-char* disemvowel(const char* str) {
-    char* result = (char*)malloc(strlen(str) + 1);
-    int j = 0;
-
-    for (int i = 0; str[i] != '\0'; i++) {
-        if (str[i] != 'a' && str[i] != 'e' && str[i] != 'i' && str[i] != 'o' && str[i] != 'u' &&
-            str[i] != 'A' && str[i] != 'E' && str[i] != 'I' && str[i] != 'O' && str[i] != 'U') {
-            result[j++] = str[i];
-        }
-    }
-
-    result[j] = '\0';
-    return result;
+char *disemvowel(const char *s)
+{
+  char * ret = calloc(strlen(s) + 1, 1);
+  for (char *q = ret; *s; s++) if (!strchr("aeiouAEIOU", *s)) *q++ = *s;
+  return ret;
 }
