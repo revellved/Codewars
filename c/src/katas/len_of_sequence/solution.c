6bb174d2dca58e78c04fb063a4d6dd0f68c984ba
Ruslan D. Revellved <revellved@gmail.com>
2024-06-27 09:46:11 +0500

    (C) 🍩 Update src/katas/len_of_sequence/solution.c

--- c/src/katas/len_of_sequence/solution.c
+++ c/src/katas/len_of_sequence/solution.c
@@ -1,24 +1,18 @@
 #include <stddef.h>
 
-size_t sequence_length(size_t n, const int numbers[], int elem) {
-    int count = 0;
-    int startIndex = -1;
-    int endIndex = -1;
+#include <stddef.h>
 
-    for (int i = 0; i < n; i++) {
-        if (numbers[i] == elem) {
-            if (startIndex == -1) {
-                startIndex = i;
-            } else {
-                endIndex = i;
-            }
-            count++;
-        }
-    }
-
-    if (count != 2) {
-        return 0;
-    }
-
-    return endIndex - startIndex + 1;
+size_t sequence_length (size_t n, const int numbers[n], int elem)
+{
+  int reps = 0;
+  size_t size = 0;
+  for (size_t i = 0; i < n; ++i) {
+    if (numbers[i] == elem)
+      ++reps;
+    if (reps > 0)
+      ++size;
+    if (reps > 1 && numbers[i] != elem)
+      --size;
+  }
+  return reps == 2 ? size: 0;
 }
