4f81813004d0dc78775af754fe943144b9c554d0
Ruslan D. Revellved <revellved@gmail.com>
2024-06-30 12:07:29 +0500

    (C) 🍩 Find Middle El

--- c/src/katas/find_middle_el/solution.c
+++ c/src/katas/find_middle_el/solution.c
@@ -1,5 +1,4 @@
-int gimme(const int tr[3])
-{
+int gimme(const int tr[3]) {
   if ((tr[0] < tr[1] && tr[1] < tr[2]) || (tr[2] < tr[1] && tr[1] < tr[0])) return 1;
   else if ((tr[1] < tr[0] && tr[0] < tr[2]) || (tr[2] < tr[0] && tr[0] < tr[1])) return 0;
   else return 2;
