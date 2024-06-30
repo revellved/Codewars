daa840e7c3c338e51769541db3545713cb11218b
Ruslan D. Revellved <revellved@gmail.com>
2024-06-30 12:07:19 +0500

    (C) 🍩 Disemvowel Trolls

--- c/src/katas/disemvowel_trolls/solution.c
+++ c/src/katas/disemvowel_trolls/solution.c
@@ -1,8 +1,7 @@
 #include <stdlib.h>
 #include <string.h>
 
-char *disemvowel(const char *s)
-{
+char *disemvowel(const char *s) {
   char * ret = calloc(strlen(s) + 1, 1);
   for (char *q = ret; *s; s++) if (!strchr("aeiouAEIOU", *s)) *q++ = *s;
   return ret;
