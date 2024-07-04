15ab0754c45b02104a03b2ec6673cd935749d7bb
Ruslan D. Revellved <revellved@gmail.com>
2024-07-04 23:04:28 +0500

    (RUBY) 🍩 Use solagraph typed instead of sorbet | Move Zeros

--- ruby/lib/katas/move_zeros/solution.rb
+++ ruby/lib/katas/move_zeros/solution.rb
@@ -2,9 +2,8 @@
 
 # https://www.codewars.com/kata/52597aa56021e91c93000cb0/ruby
 module MoveZeros
-  extend T::Sig
-
-  sig { params(arr: T::Array[Integer]).returns(T::Array[Integer]) }
+  # @param arr [Array<Integer>]
+  # @return [Array<Integer>]
   def moveZeros(arr) 
     arr.sort_by { |w| w == 0 ? 1 : 0 }
   end
