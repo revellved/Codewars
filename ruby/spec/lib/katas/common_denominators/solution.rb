15eb112611ec959833c86b15c1137a4f24b7865c
Ruslan D. Revellved <revellved@gmail.com>
2024-07-04 23:22:04 +0500

    (RUBY) 🍩 Extend to include and fix code style in specs | Common Denominators

--- ruby/spec/lib/katas/common_denominators/solution.rb
+++ ruby/spec/lib/katas/common_denominators/solution.rb
@@ -1,9 +1,9 @@
 # frozen_string_literal: true
 
 describe 'Basic tests' do
-  it 'should pass basic tests' do
-    extend CommonDeniminators
+  include CommonDeniminators
 
+  it 'should pass basic tests' do
     expect(convert_fracts([[1, 2], [1, 3], [1, 4]])).to eq([[6, 12], [4, 12], [3, 12]])
   end
 end
