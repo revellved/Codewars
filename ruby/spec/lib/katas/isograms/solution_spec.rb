95b979b01f3f0580af6ff4c296f2452fc41e1477
Ruslan D. Revellved <revellved@gmail.com>
2024-07-04 23:22:26 +0500

    (RUBY) 🍩 Extend to include and fix code style in specs | Isograms

--- ruby/spec/lib/katas/isograms/solution_spec.rb
+++ ruby/spec/lib/katas/isograms/solution_spec.rb
@@ -1,12 +1,12 @@
-describe "Solution" do
-  it "Fixed tests" do
-    extend Isograms
+describe 'Solution' do
+  include Isograms
 
-    Test.assert_equals(is_isogram("Dermatoglyphics"), true )
-    Test.assert_equals(is_isogram("isogram"), true )
-    Test.assert_equals(is_isogram("aba"), false, "same chars may not be adjacent" )
-    Test.assert_equals(is_isogram("moOse"), false, "same chars may not be same case" )
-    Test.assert_equals(is_isogram("isIsogram"), false )
-    Test.assert_equals(is_isogram(""), true, "an empty string is a valid isogram" )
+  it 'Fixed tests' do
+    Test.assert_equals(is_isogram('Dermatoglyphics'), true)
+    Test.assert_equals(is_isogram('isogram'), true)
+    Test.assert_equals(is_isogram('aba'), false, 'same chars may not be adjacent')
+    Test.assert_equals(is_isogram('moOse'), false, 'same chars may not be same case')
+    Test.assert_equals(is_isogram('isIsogram'), false)
+    Test.assert_equals(is_isogram(''), true, 'an empty string is a valid isogram')
   end
 end
