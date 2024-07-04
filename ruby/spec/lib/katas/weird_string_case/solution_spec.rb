420bb47393dd076d85ef796c3c0a962001b53d4b
Ruslan D. Revellved <revellved@gmail.com>
2024-07-04 23:23:48 +0500

    (RUBY) 🍩 Extend to include and fix code style in specs | Weird String Case

--- ruby/spec/lib/katas/weird_string_case/solution_spec.rb
+++ ruby/spec/lib/katas/weird_string_case/solution_spec.rb
@@ -1,16 +1,14 @@
 # frozen_string_literal: true
 
 describe 'weirdcase' do
-  it 'should return the correct value for a single word' do
-    extend WeirdStringCase
+  include WeirdStringCase
 
+  it 'should return the correct value for a single word' do
     Test.assert_equals(weirdcase('This'), 'ThIs')
     Test.assert_equals(weirdcase('is'), 'Is')
   end
 
   it 'should return the correct value for multiple words' do
-    extend WeirdStringCase
-
     Test.assert_equals(weirdcase('This is a test'), 'ThIs Is A TeSt')
   end
 end
