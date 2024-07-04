920137c8628599222181cddb8923e28ccefeb3b6
Ruslan D. Revellved <revellved@gmail.com>
2024-07-04 23:22:17 +0500

    (RUBY) 🍩 Extend to include and fix code style in specs | Descending Order

--- ruby/spec/lib/katas/descending_order/solution_spec.rb
+++ ruby/spec/lib/katas/descending_order/solution_spec.rb
@@ -1,9 +1,9 @@
 # frozen_string_literal: true
 
 describe 'Solution' do
-  it 'Fixed tests' do
-    extend DescendingOrder
+  include DescendingOrder
 
+  it 'Fixed tests' do
     Test.assert_equals(descending_order(0), 0)
     Test.assert_equals(descending_order(1), 1)
     Test.assert_equals(descending_order(123_456_789), 987_654_321)
