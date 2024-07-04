d717ab622795a0d58167a9fbc6cff4fe85262202
Ruslan D. Revellved <revellved@gmail.com>
2024-07-04 23:04:45 +0500

    (RUBY) 🍩 Use solagraph typed instead of sorbet | Number Prime

--- ruby/lib/katas/number_prime/solution.rb
+++ ruby/lib/katas/number_prime/solution.rb
@@ -2,9 +2,8 @@
 
 # https://www.codewars.com/kata/5467e4d82edf8bbf40000155/ruby
 module NumberPrime
-  extend T::Sig
-
-  sig { params(num: Integer).returns(T::Boolean) }
+  # @param num [Integer]
+  # @return [Boolean]
   def prime?(num)
     return false if num <= 1
 
