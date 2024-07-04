49931ed75921f3a1a7dc24dc0995b2293749bff2
Ruslan D. Revellved <revellved@gmail.com>
2024-07-04 23:05:01 +0500

    (RUBY) 🍩 Use solagraph typed instead of sorbet | Roman Numerals decoder

--- ruby/lib/katas/roman_numerals_decoder/solution.rb
+++ ruby/lib/katas/roman_numerals_decoder/solution.rb
@@ -2,14 +2,13 @@
 
 # https://www.codewars.com/kata/51b6249c4612257ac0000005/ruby
 module RomanNumeralsDecoder
-  extend T::Sig
-
-  sig { returns(T::Hash[String, Integer]) }
+  # @return [Hash<String, Integer>]
   def roman_dict
     { 'M' => 1000, 'D' => 500, 'C' => 100, 'L' => 50, 'X' => 10, 'V' => 5, 'I' => 1 }
   end
 
-  sig { params(roman: String).returns(Integer) }
+  # @param roman [String]
+  # @return [Integer]
   def solution(roman)
     roman.chars.map { |c| roman_dict[c] }.reduce { |a, b| a < b ? b - a : a + b }
   end
