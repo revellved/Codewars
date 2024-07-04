b051a58aa4fae97a1bb55f0ef76c6419c9b98f00
Ruslan D. Revellved <revellved@gmail.com>
2024-07-04 23:04:52 +0500

    (RUBY) 🍩 Use solagraph typed instead of sorbet | Odd or even

--- ruby/lib/katas/odd_or_even/solution.rb
+++ ruby/lib/katas/odd_or_even/solution.rb
@@ -2,10 +2,10 @@
 
 # https://www.codewars.com/kata/5949481f86420f59480000e7/ruby
 module OddOrEven
-  extend T::Sig
-
-  sig { params(array: T::Array[Integer]).returns(String) }
+  # @param [Array<Integer>]
+  # @return [String]
   def odd_or_even(array)
     array.sum.odd? ? 'odd' : 'even'
+    array.
   end
 end
