80fc96cdcc24b65f29b9ef1709fb4dc224226581
Ruslan D. Revellved <revellved@gmail.com>
2024-07-04 23:03:57 +0500

    (RUBY) 🍩 Use solagraph typed instead of sorbet | Delete Nth

--- ruby/lib/katas/delete_nth/solution.rb
+++ ruby/lib/katas/delete_nth/solution.rb
@@ -2,9 +2,9 @@
 
 # https://www.codewars.com/kata/554ca54ffa7d91b236000023/ruby
 module DeleteNth
-  extend T::Sig
-
-  sig { params(order: T::Array[Integer], max_e: Integer).returns(T::Array[Integer]) }
+  # @param order [Array<Integer>]
+  # @param max_e [Integer]
+  # @return [Array<Integer>]
   def delete_nth(order, max_e)
     occurrences = Hash.new(0)
     order.reject { |item| (occurrences[item] += 1) > max_e }
