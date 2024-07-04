c7b3bfc2824b3d360f6403489d220a08972a9034
Ruslan D. Revellved <revellved@gmail.com>
2024-07-04 23:12:51 +0500

    (RUBY) 🍩 Fix error

--- ruby/lib/katas/odd_or_even/solution.rb
+++ ruby/lib/katas/odd_or_even/solution.rb
@@ -2,10 +2,9 @@
 
 # https://www.codewars.com/kata/5949481f86420f59480000e7/ruby
 module OddOrEven
-  # @param [Array<Integer>]
+  # @param array [Array<Integer>]
   # @return [String]
   def odd_or_even(array)
     array.sum.odd? ? 'odd' : 'even'
-    array.
   end
 end
