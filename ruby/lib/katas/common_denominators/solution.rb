2bddcdf4b9a1fe3ab076c2f5482c998fc9a793e1
Ruslan D. Revellved <revellved@gmail.com>
2024-07-04 23:03:46 +0500

    (RUBY) 🍩 Use solagraph typed instead of sorbet | Common Denominators

--- ruby/lib/katas/common_denominators/solution.rb
+++ ruby/lib/katas/common_denominators/solution.rb
@@ -2,11 +2,10 @@
 
 # https://www.codewars.com/kata/54d7660d2daf68c619000d95/ruby
 module CommonDeniminators
-  extend T::Sig
-
-  sig { params(number: T::Array[[Integer, Integer]]).returns(T::Array[[Integer, Integer]]) }
+  # @param lst [Array<Array<Integer>>]
+  # @return [Array<Array<Integer>>]
   def convert_fracts(lst)
-    denominators = lst.map { |frac| frac[1] }
+    denominators = lst.map { |frac| frac[0] }
     lcm = denominators.reduce(1, :lcm)
 
     lst.map { |frac| [frac[0] * (lcm / frac[1]), lcm] }
