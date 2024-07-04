bc3584726237d905f3b549dd88d898724ff6f91d
Ruslan D. Revellved <revellved@gmail.com>
2024-07-04 23:05:13 +0500

    (RUBY) 🍩 Use solagraph typed instead of sorbet | Two Sum

--- ruby/lib/katas/two_sum/solution.rb
+++ ruby/lib/katas/two_sum/solution.rb
@@ -2,11 +2,10 @@
 
 # https://www.codewars.com/kata/52c31f8e6605bcc646000082/ruby
 module TwoSum
-  extend T::Sig
-
-  sig { params(numbers: T::Array[Integer], target: Integer).returns(T::Array[Integer]) }
+  # @param numbers [Array<Integer>]
+  # @return [Array<Integer>]
   def two_sum(numbers, target)
-    numbers.each_with_index.to_a.combination(2) do |arr| 
+    numbers.each_with_index.to_a.combination(2) do |arr|
       return [arr[0][1], arr[1][1]] if arr[0][0] + arr[1][0] == target
     end
   end
