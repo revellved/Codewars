f82128708301e85f1f7adc13f657354c02394349
Ruslan D. Revellved <revellved@gmail.com>
2024-07-04 23:05:08 +0500

    (RUBY) 🍩 Use solagraph typed instead of sorbet | Roman Numerals Encoder

--- ruby/lib/katas/roman_numerals_encoder/solution.rb
+++ ruby/lib/katas/roman_numerals_encoder/solution.rb
@@ -2,9 +2,7 @@
 
 # https://www.codewars.com/kata/51b62bf6a9c58071c600001b/ruby
 module RomanNumeralsEncoder
-  extend T::Sig
-
-  sig { returns(T::Hash[String, Integer]) }
+  # @return [Hash<String, Integer>]
   def numerals
     {
       'M' => 1000, 'CM' => 900, 'D' => 500, 'CD' => 400, 'C' => 100,
@@ -13,7 +11,8 @@ module RomanNumeralsEncoder
     }
   end
 
-  sig { params(number: Integer).returns(String) }
+  # @param number [Integer]
+  # @return [String]
   def solution(number)
     return '' if number <= 0
 
