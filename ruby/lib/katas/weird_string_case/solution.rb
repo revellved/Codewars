f606c2fa2a1ce26439d80793e3a17ab1257a3277
Ruslan D. Revellved <revellved@gmail.com>
2024-07-04 23:05:26 +0500

    (RUBY) 🍩 Use solagraph typed instead of sorbet | Weird String

--- ruby/lib/katas/weird_string_case/solution.rb
+++ ruby/lib/katas/weird_string_case/solution.rb
@@ -2,9 +2,8 @@
 
 # https://www.codewars.com/kata/52b757663a95b11b3d00062d/ruby
 module WeirdStringCase
-  extend T::Sig
-
-  sig { params(string: String).returns(String) }
+  # @param string [String]
+  # @return [String]
   def weirdcase(string)
     string.gsub(/\w{1,2}/) { ::Regexp.last_match(0).capitalize }
   end
