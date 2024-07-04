74e89fecedafad0ee57731dc95c33433765bd8a8
Ruslan D. Revellved <revellved@gmail.com>
2024-07-04 23:04:22 +0500

    (RUBY) 🍩 Use solagraph typed instead of sorbet | Isograms

--- ruby/lib/katas/isograms/solution.rb
+++ ruby/lib/katas/isograms/solution.rb
@@ -2,9 +2,8 @@
 
 # https://www.codewars.com/kata/54ba84be607a92aa900000f1/ruby
 module Isograms
-  extend T::Sig
-
-  sig { params(string: String).returns(T::Boolean) }
+  # @param string [String]
+  # @return [Boolean]
   def is_isogram(string)
     !string.downcase.chars.uniq!
   end
