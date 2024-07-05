703a7e7244412f4ddc456d3c4a8776d4338a1365
Ruslan D. Revellved <revellved@gmail.com>
2024-07-05 11:00:09 +0500

    (RUBY) 🍩 Fix style code for specs | Delete Nth

--- ruby/spec/lib/katas/delete_nth/solution_spec.rb
+++ ruby/spec/lib/katas/delete_nth/solution_spec.rb
@@ -3,7 +3,7 @@
 describe 'Delete Nth spec solution' do
   include DeleteNth
 
-  it 'Basic tests' do
+  it 'Fixed tests' do
     expect(delete_nth([20, 37, 20, 21], 1)).to eq([20, 37, 21]), 'max_e = 1'
     expect(delete_nth([1, 1, 3, 3, 7, 2, 2, 2, 2], 3)).to eq([1, 1, 3, 3, 7, 2, 2, 2]), 'max_e =3'
     expect(delete_nth([12, 39, 19, 39, 39, 19, 12], 1)).to eq([12, 39, 19]), 'max_e = 1'
