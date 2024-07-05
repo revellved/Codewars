38de28c56186fef8b44dca5b913f21349a8ed18c
Ruslan D. Revellved <revellved@gmail.com>
2024-07-05 10:55:07 +0500

    (RUBY) 🍩 Fix style code for specs | Roman Numeral Two Sum

--- ruby/spec/lib/katas/two_sum/solution_spec.rb
+++ ruby/spec/lib/katas/two_sum/solution_spec.rb
@@ -1,12 +1,11 @@
 # frozen_string_literal: true
 
-describe 'Solution' do
+describe 'Two Sum spec solution' do
   it 'Fixed tests' do
     extend TwoSum
 
     Test.assert_equals(two_sum([1, 2, 3], 4).sort, [0, 2])
-    Test.assert_equals(two_sum([1234, 5678, 9012], 14690).sort, [1, 2])
+    Test.assert_equals(two_sum([1234, 5678, 9012], 14_690).sort, [1, 2])
     Test.assert_equals(two_sum([2, 2, 3], 4).sort, [0, 1])
   end
 end
-
