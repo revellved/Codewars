a396068b16677e37e1c8cfa88d29a969e3ee9013
Ruslan D. Revellved <revellved@gmail.com>
2024-07-05 11:00:20 +0500

    (RUBY) 🍩 Fix style code for specs | Number Prime

--- ruby/spec/lib/katas/number_prime/solution_spec.rb
+++ ruby/spec/lib/katas/number_prime/solution_spec.rb
@@ -3,19 +3,21 @@
 describe 'Number Prime spec solution' do
   include NumberPrime
 
-  it 'Should return false for non-prime numbers.' do
-    Test.assert_equals(prime?(4), false)
-    Test.assert_equals(prime?(100), false)
-    Test.assert_equals(prime?(999), false)
-    Test.assert_equals(prime?(958_297), false)
-    Test.assert_equals(prime?(-7), false)
-  end
+  context 'Fixed tests' do
+    it 'Should return false for non-prime numbers.' do
+      Test.assert_equals(prime?(4), false)
+      Test.assert_equals(prime?(100), false)
+      Test.assert_equals(prime?(999), false)
+      Test.assert_equals(prime?(958_297), false)
+      Test.assert_equals(prime?(-7), false)
+    end
 
-  it 'Should return true for prime numbers.' do
-    Test.assert_equals(prime?(2), true)
-    Test.assert_equals(prime?(3), true)
-    Test.assert_equals(prime?(5), true)
-    Test.assert_equals(prime?(457), true)
-    Test.assert_equals(prime?(39_229), true)
+    it 'Should return true for prime numbers.' do
+      Test.assert_equals(prime?(2), true)
+      Test.assert_equals(prime?(3), true)
+      Test.assert_equals(prime?(5), true)
+      Test.assert_equals(prime?(457), true)
+      Test.assert_equals(prime?(39_229), true)
+    end
   end
 end
