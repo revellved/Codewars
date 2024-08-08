cdd1ce56f9b1fc724dd24168755c667ccab392d8
Ruslan D. Revellved <revellved@gmail.com>
2024-08-08 22:15:22 +0500

    (RUST) 🍩 Update Cargo.lock Cargo.toml src/modules/num_get_index/mod.rs     🍩 Delete ../new/.gitignore ../new/.mocharc.json ../new/.npmrc ../new/Makefile ../new/README.md ../new/package-lock.json ../new/package.json ../new/src/index.ts ../new/src/katas/basic_bounding_box/Readme.md ../new/src/katas/basic_bounding_box/solution.spec.ts ../new/src/katas/basic_bounding_box/solution.ts ../new/src/katas/dup_encoder/Readme.md ../new/src/katas/dup_encoder/solution.spec.ts ../new/src/katas/dup_encoder/solution.ts ../new/src/katas/number_big/Readme.md ../new/src/katas/number_big/solution.spec.ts ../new/src/katas/number_big/solution.ts ../new/src/katas/supermarket_queue/Readme.md ../new/src/katas/supermarket_queue/solution.spec.ts ../new/src/katas/supermarket_queue/solution.ts ../new/src/katas/tribonacci_sequence/Readme.md ../new/src/katas/tribonacci_sequence/solution.spec.ts ../new/src/katas/tribonacci_sequence/solution.ts ../new/test.tsconfig.json ../new/tsconfig.json

--- rust/src/modules/num_get_index/mod.rs
+++ rust/src/modules/num_get_index/mod.rs
@@ -1,18 +1,7 @@
-use num_traits::PrimInt;
-//
-// pub fn num_get_index<N: PrimInt>(n: N, i: u32) -> Option<N> {
-//     let div = 10u32.pow(i - 1);
-//     let div_right: N = N::from(div).unwrap();
-//     let div_left: N = N::from(div * 10).unwrap();
-//     match div_right > n {
-//         true => None,
-//         false => Some((n % div_left - (n % div_right)) / div_right),
-//     }
-// }
-pub fn num_get_index<N: PrimInt>(n: N, i: u32) -> Option<N> {
+pub fn num_get_index(n: u32, i: u32) -> Option<u32> {
     let div = 10u32.pow(i - 1);
-    let div_right: N = N::from(div).unwrap();
-    let div_left: N = N::from(div * 10).unwrap();
+    let div_right = div;
+    let div_left = div * 10;
     match div_right > n {
         true => None,
         false => Some((n % div_left - (n % div_right)) / div_right),
