270fc742540c491f9893e6a417f78f8ec1dd391c
Ruslan D. Revellved <revellved@gmail.com>
2024-08-08 22:15:37 +0500

    (CODEWARS)     🍩 Delete new/.gitignore new/.mocharc.json new/.npmrc new/Makefile new/README.md new/package-lock.json new/package.json new/src/index.ts new/src/katas/basic_bounding_box/Readme.md new/src/katas/basic_bounding_box/solution.spec.ts new/src/katas/basic_bounding_box/solution.ts new/src/katas/dup_encoder/Readme.md new/src/katas/dup_encoder/solution.spec.ts new/src/katas/dup_encoder/solution.ts new/src/katas/number_big/Readme.md new/src/katas/number_big/solution.spec.ts new/src/katas/number_big/solution.ts new/src/katas/supermarket_queue/Readme.md new/src/katas/supermarket_queue/solution.spec.ts new/src/katas/supermarket_queue/solution.ts new/src/katas/tribonacci_sequence/Readme.md new/src/katas/tribonacci_sequence/solution.spec.ts new/src/katas/tribonacci_sequence/solution.ts new/test.tsconfig.json new/tsconfig.json

--- new/src/katas/dup_encoder/solution.ts
+++
@@ -1,9 +0,0 @@
-export function duplicateEncode(word: string): string {
-  return word
-    .toLowerCase()
-    .split('')
-    .map( function (a, _, w) {
-      return w.indexOf(a) == w.lastIndexOf(a) ? '(' : ')'
-    })
-    .join('');
-}
--- new/src/katas/supermarket_queue/solution.ts
+++
@@ -1,8 +0,0 @@
-export function queueTime(customers: number[], n: number): number {
-    var w = new Array(n).fill(0);
-    for (let t of customers) {
-        let idx = w.indexOf(Math.min(...w));
-        w[idx] += t;
-    }
-    return Math.max(...w);
-}
